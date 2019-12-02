7b7ca5224303ab3c8a13b202705ebc211f4f0d41
GuGU <wnduddl3409@naver.com>
2019-12-02 11:15:30 +0900

    [FIX] 시퀄라이즈 객체 오류 수정

--- server/database/models/index.js
+++ server/database/models/index.js
@@ -1,16 +1,15 @@
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require('../config/dbconfig')[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize( config.username, config.database, config.password, config );
 
-db.User = require('./user')(sequelize,Sequelize);
+db.User = require('./user')(sequelize, Sequelize);
 
-// 변경 필요
-// const config = require(path.join(__dirname, '..', 'config', 'dbconfig.json'))['test'];
+// add db object associate
 
-// const user = User(sequelize, Sequelize);
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-// export { sequelize, Sequelize, user };
-
+module.exports = db;
