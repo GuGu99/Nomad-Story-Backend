a59ad74aa7d6d4879a65ff650bbf140a86094c19
GuGU <wnduddl3409@naver.com>
2019-12-02 09:47:09 +0900

    [DELETE] 기존 파일 삭제
    기존 파일 삭제

--- src/api/auth/index.js
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-import * as authController from "./auth.ctrl";
-
-const auth = new Router();
-
-auth.post('/signup', authController.signup);
-auth.post('/signin', authController.signin);
-
-export default auth;
